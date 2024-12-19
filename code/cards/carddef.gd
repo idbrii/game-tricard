f5afdeec47b88812df7cf0d66b360a7cd19d67b1
David Briscoe <idbrii@gmail.com>
2024-12-18 22:05:27 -0800

    Add _init in CardDef
    Fix selecting CardDefs doesn't actually show their contents -- it just
    continues to show the previous one.
    
    I don't know why we have to implement this boilerplate _init in every
    Resource.

--- code/cards/carddef.gd
+++ code/cards/carddef.gd
@@ -3,6 +3,8 @@ class_name CardDef
 
 @export var card_name := ""
 
+@export var art := CompressedTexture2D
+
 ## Does the card attack all enemies?
 @export var is_barrage := false
 
@@ -11,4 +13,16 @@ class_name CardDef
 ## Three values that are the power values on each chamber.
 @export var chamber_values : Array[int]
 
+# Resources don't display properly without this boilerplate init function.
+func _init(
+        p_card_name = "",
+        p_art = null,
+        p_is_barrage = false,
+        p_actions : Array[PackedScene] = [],
+        p_chamber_values : Array[int] = []):
 
+    card_name = p_card_name
+    art = p_art
+    is_barrage = p_is_barrage
+    actions = p_actions
+    chamber_values = p_chamber_values
