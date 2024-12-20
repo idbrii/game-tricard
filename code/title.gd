415f6dd76c2d6c56a7e2e3fcac341465f4cd382c
David Briscoe <idbrii@gmail.com>
2024-12-20 15:03:40 -0800

    Hookup startup screen

--- code/title.gd
+++ code/title.gd
@@ -1,7 +1,11 @@
 extends Node3D
 
 func _ready() -> void:
-	pass # Replace with function body.
+	if $AnimationPlayer:
+		$AnimationPlayer.play("intro")
+		await $AnimationPlayer.animation_finished
+		get_tree().change_scene_to_file("res://scenes/title.tscn")
+
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("click"):
