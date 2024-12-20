0533ba831b7b5e4b62e605fcec0e4f15ee3de032
David Briscoe <idbrii@gmail.com>
2024-12-20 11:51:46 -0800

    Revert "in the battle level"
    "There will be an animation player that has the 'camera title card'
    position and a 'title card to battle field anim.'  'Reset' will bring
    the camera back to its defaul place."
    
    This reverts commit 417bf61f6f5be8234397abb8fd8e0a9ee2bb300f.
    
    Kial says it's easier to just make this fix.

--- scenes/battle.gd
+++ scenes/battle.gd
@@ -10,24 +10,24 @@ var PrefabSnake = preload("res://scenes/enemies/snake.tscn")
 var PrefabBossilisk = preload("res://scenes/enemies/bossilisk.tscn")
 
 enum Mode {
-	None,
-	Intro,
-	Select,
-	SelectAll,
-	PlayerTurn,
-	EnemyTurn,
+    None,
+    Intro,
+    Select,
+    SelectAll,
+    PlayerTurn,
+    EnemyTurn,
 }
 
 var mode:Mode
 var currentSelection:Enemy
 var enemies:Dictionary
 var enemy_types:Array = [
-	PrefabCowboy,
-	PrefabGoblin,
-	PrefabDemon,
-	PrefabFireBat,
-	PrefabKillyote,
-	PrefabSnake,
+    PrefabCowboy,
+    PrefabGoblin,
+    PrefabDemon,
+    PrefabFireBat,
+    PrefabKillyote,
+    PrefabSnake,
 ]
 var killCount:int = 0
 @export var reqKillCount:int = 5
@@ -35,103 +35,103 @@ var killCount:int = 0
 signal enemyPicked
 
 func _ready() -> void:
-	mode = Mode.Intro
-	await spawnEnemies()
-	mode = Mode.None
+    mode = Mode.Intro
+    await spawnEnemies()
+    mode = Mode.None
 
 func _process(_delta: float) -> void:
-	if mode == Mode.Select:
-		var en = getEnemyAtMouse()
-		set_enemy_focus(en)
-		if en != null and Input.is_action_just_pressed("click"):
-			mode = Mode.None
-			set_enemy_focus(null)
-			self.enemyPicked.emit(en)
-	elif mode == Mode.SelectAll:
-		var en = getEnemyAtMouse()
-		if en:
-			if Input.is_action_just_pressed("click"):
-				self.enemyPicked.emit(en)
-			else:
-				for victim: Enemy in enemies.values():
-					victim.focus()
-		else:
-			for victim: Enemy in enemies.values():
-				victim.unfocus()
+    if mode == Mode.Select:
+        var en = getEnemyAtMouse()
+        set_enemy_focus(en)
+        if en != null and Input.is_action_just_pressed("click"):
+            mode = Mode.None
+            set_enemy_focus(null)
+            self.enemyPicked.emit(en)
+    elif mode == Mode.SelectAll:
+        var en = getEnemyAtMouse()
+        if en:
+            if Input.is_action_just_pressed("click"):
+                self.enemyPicked.emit(en)
+            else:
+                for victim: Enemy in enemies.values():
+                    victim.focus()
+        else:
+            for victim: Enemy in enemies.values():
+                victim.unfocus()
 
 
 func set_enemy_focus(en:Enemy):
-	if en != currentSelection:
-		if currentSelection != null:
-			currentSelection.unfocus()
-			prints(currentSelection.name, "lost focus")
-		if en != null:
-			prints(en.name, "in focus")
-			en.focus()
-	currentSelection = en
+    if en != currentSelection:
+        if currentSelection != null:
+            currentSelection.unfocus()
+            prints(currentSelection.name, "lost focus")
+        if en != null:
+            prints(en.name, "in focus")
+            en.focus()
+    currentSelection = en
 
 func spawn(type, n:Node3D):
-	var en:Enemy = type.instantiate()
-	en.position = n.position
-	en.spawner = n.name
-	en.disposable.connect(_on_enemy_disposable)
-	en.died.connect(_on_enemy_died)
-	en.visible = false
-	enemies[n.name] = en
-	add_child(en)
-	en.spawn()
-	print("spawned enemy at ", n.name)
+    var en:Enemy = type.instantiate()
+    en.position = n.position
+    en.spawner = n.name
+    en.disposable.connect(_on_enemy_disposable)
+    en.died.connect(_on_enemy_died)
+    en.visible = false
+    enemies[n.name] = en
+    add_child(en)
+    en.spawn()
+    print("spawned enemy at ", n.name)
 
 func spawnEnemies():
-	randomize()
-	var isBossOut = enemies.has($BossSpawnPoint.name)
-	var shouldSpawnBoss = killCount >= reqKillCount and !isBossOut
-	for n:Node3D in $SpawnPoints.get_children():
-		if enemies.has(n.name):
-			continue
-		if n.name == "B" and (isBossOut or shouldSpawnBoss):
-			continue
-		var i:int = randi() % enemy_types.size()
-		var type = enemy_types[i]
-		spawn(type, n)
-	# Special spawn boss logic
-	if shouldSpawnBoss:
-		spawn(PrefabBossilisk, $BossSpawnPoint)
+    randomize()
+    var isBossOut = enemies.has($BossSpawnPoint.name)
+    var shouldSpawnBoss = killCount >= reqKillCount and !isBossOut
+    for n:Node3D in $SpawnPoints.get_children():
+        if enemies.has(n.name):
+            continue
+        if n.name == "B" and (isBossOut or shouldSpawnBoss):
+            continue
+        var i:int = randi() % enemy_types.size()
+        var type = enemy_types[i]
+        spawn(type, n)
+    # Special spawn boss logic
+    if shouldSpawnBoss:
+        spawn(PrefabBossilisk, $BossSpawnPoint)
 
 func selectEnemy():
-	mode = Mode.Select
+    mode = Mode.Select
 
 func startEnemyTurn(player:Player):
-	mode = Mode.EnemyTurn
-	var que = enemies.values()
-	for n:Enemy in que:
-		if n.status.is_dead():
-			continue
-		await n.tick(player)
-	await get_tree().create_timer(0.2).timeout
-	await spawnEnemies()
-	mode = Mode.None
+    mode = Mode.EnemyTurn
+    var que = enemies.values()
+    for n:Enemy in que:
+        if n.status.is_dead():
+            continue
+        await n.tick(player)
+    await get_tree().create_timer(0.2).timeout
+    await spawnEnemies()
+    mode = Mode.None
 
 func _on_enemy_died(en:Enemy):
-	print("deregistering enemy")
-	killCount += 1
-	enemies.erase(en.spawner)
-	# TODO if boss died then let's do game over!
+    print("deregistering enemy")
+    killCount += 1
+    enemies.erase(en.spawner)
+    # TODO if boss died then let's do game over!
 
 func _on_enemy_disposable(en:Enemy):
-	print("disposing enemy")
-	remove_child(en)
+    print("disposing enemy")
+    remove_child(en)
 
 func getEnemyAtMouse(ray_len:int = 1000) -> Enemy:
-	var space_state = get_world_3d().direct_space_state
-	var cam = $Camera
-	var mousepos = get_viewport().get_mouse_position()
-	var origin = cam.project_ray_origin(mousepos)
-	var end = origin + cam.project_ray_normal(mousepos) * ray_len
-	var query = PhysicsRayQueryParameters3D.create(origin, end)
-	query.collide_with_areas = true
-	var result = space_state.intersect_ray(query)
-	if result.is_empty():
-		return null
-	# TODO check if is Enemy
-	return result.collider
+    var space_state = get_world_3d().direct_space_state
+    var cam = $Camera
+    var mousepos = get_viewport().get_mouse_position()
+    var origin = cam.project_ray_origin(mousepos)
+    var end = origin + cam.project_ray_normal(mousepos) * ray_len
+    var query = PhysicsRayQueryParameters3D.create(origin, end)
+    query.collide_with_areas = true
+    var result = space_state.intersect_ray(query)
+    if result.is_empty():
+        return null
+    # TODO check if is Enemy
+    return result.collider
