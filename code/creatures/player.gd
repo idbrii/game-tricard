410baff04e9b57a7c46437d003d78ddb3f86a65c
Thomas Mathews <thomas.mathews@kleientertainment.com>
2024-12-19 17:28:48 -0800

    Added new enemie scenes

--- code/creatures/player.gd
+++ code/creatures/player.gd
@@ -8,36 +8,37 @@ class_name Player
 @onready var scene:Battle = self.get_parent()
 
 func _ready():
-	scene.mode = Battle.Mode.None
-	attack_btn.pressed.connect(_on_attack_pressed)
-	discard_btn.pressed.connect(_on_discard_pressed)
+    scene.mode = Battle.Mode.None
+    attack_btn.pressed.connect(_on_attack_pressed)
+    discard_btn.pressed.connect(_on_discard_pressed)
 
 
 func _on_attack_pressed():
-	if scene.mode != Battle.Mode.None:
-		return
+    if scene.mode != Battle.Mode.None:
+        return
 
-	var card := InputFocus.get_focus() as Card
-	if not card:
-		return
+    var card := InputFocus.get_focus() as Card
+    if not card:
+        return
 
-	var target = self
-	if card.requires_target():
-		scene.mode = Battle.Mode.Select
-		target = await scene.enemyPicked
+    var target = self
+    if card.requires_target():
+        prints("need target!")
+        scene.mode = Battle.Mode.Select
+        target = await scene.enemyPicked
 
-	scene.mode = Battle.Mode.PlayerTurn
-	await play_card(card, target)
-	await scene.startEnemyTurn(self)
+    scene.mode = Battle.Mode.PlayerTurn
+    await play_card(card, target)
+    await scene.startEnemyTurn(self)
 
 
 func _on_discard_pressed():
-	deck.discard_all()
+    deck.discard_all()
 
 
 func play_card(card, target):
-	prints("Playing card", card, "on", target)
-	card.play_card(self, target)
-	await get_tree().create_timer(0.6).timeout
-	# Clear the card so it goes back into the hand.
-	InputFocus.set_focus(null)
+    prints("Playing card", card, "on", target)
+    card.play_card(self, target)
+    await get_tree().create_timer(0.6).timeout
+    # Clear the card so it goes back into the hand.
+    InputFocus.set_focus(null)
--- code/creatures/status.gd
+++ code/creatures/status.gd
@@ -8,66 +8,66 @@ signal blocked(amount:int)
 
 @export var display_name:String
 ## Life force
-@export var health := 100
+@export var health:int
 ## Prevent this much damage
-@export var block := 0
+@export var block:int
 ## At end of turn, damage by this much and reduce burn by one.
-@export var burn := 0
+@export var burn:int
 ## At end of turn, reduce health by this much and reduce poison by two.
-@export var poison := 0
-@export var turns := 0
+@export var poison:int
+@export var turns:int
 @export var maxTurns:int = 1
-@export var maxHealth:int = 100
+@export var maxHealth:int = 3
 @export var maxBlocks:int = 1
 
 func reset():
-	health = maxHealth
-	turns = maxTurns
-	block = 0
-	poison = 0
-	burn = 0
+    health = maxHealth
+    turns = maxTurns
+    block = 0
+    poison = 0
+    burn = 0
 
 func is_dead() -> bool:
-	return health <= 0
+    return health <= 0
 
 func end_turn():
-	if burn > 0:
-		mod_health(-burn)
-		mod_burn(-1)
-	if poison > 0:
-		mod_health(-poison, true)
-		mod_poison(-2)
+    if burn > 0:
+        mod_health(-burn)
+        mod_burn(-1)
+    if poison > 0:
+        mod_health(-poison, true)
+        mod_poison(-2)
 
 func mod_health(amount, ignore_block = false):
-	if not ignore_block and amount < 0 and block > 0:
-		var before = amount
-		var used_block = min(-amount, block)
-		amount += used_block
-		block -= used_block
-		blocked.emit(used_block)
-	health += amount
-	if health <= 0:
-		die.emit()
-	elif amount < 0:
-		hurt.emit(amount)
+    if not ignore_block and amount < 0 and block > 0:
+        var before = amount
+        var used_block = min(-amount, block)
+        amount += used_block
+        block -= used_block
+        blocked.emit(used_block)
+    health += amount
+    if health <= 0:
+        die.emit()
+    elif amount < 0:
+        hurt.emit(amount)
 
 func _mod_stat(stat, amount):
-	stat += amount
-	stat = max(stat, 0)
-	return stat
+    stat += amount
+    stat = max(stat, 0)
+    return stat
 
 func mod_block(amount):
-	block = _mod_stat(block, amount)
+    block = _mod_stat(block, amount)
 
 func mod_burn(amount):
-	burn = _mod_stat(burn, amount)
+    burn = _mod_stat(burn, amount)
 
 func mod_poison(amount):
-	poison = _mod_stat(poison, amount)
+    poison = _mod_stat(poison, amount)
 
 func mod_turns(amount):
-	turns += amount
-	turned.emit(turns)
+    turns += amount
+    turned.emit(turns)
 
 func reset_turns():
-	turns = maxTurns
+    turns = maxTurns
