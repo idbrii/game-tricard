6cc97d8b25d2c69136d004ebf3f7926a9e55082b
David Briscoe <idbrii@gmail.com>
2024-12-20 20:43:10 -0800

    Fix crash on startup
    This was preventing cards from getting dealt so all builds were broken.

--- code/title.gd
+++ code/title.gd
@@ -1,12 +1,12 @@
 extends Node3D
 
 func _ready() -> void:
-	if $AnimationPlayer:
+	if has_node("AnimationPlayer"):
 		$AnimationPlayer.play("intro")
 		await $AnimationPlayer.animation_finished
 		get_tree().change_scene_to_file("res://scenes/title.tscn")
 
 
-func _process(delta: float) -> void:
+func _process(_dt: float) -> void:
 	if Input.is_action_just_pressed("click"):
 		get_tree().change_scene_to_file("res://scenes/battle.tscn")
