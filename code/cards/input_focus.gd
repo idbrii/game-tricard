2a636ce4611774dcf9be3c28a05ad52cad72651a
David Briscoe <idbrii@gmail.com>
2024-12-20 20:47:37 -0800

    Support fullscreen and quit buttons

--- code/cards/input_focus.gd
+++ code/cards/input_focus.gd
@@ -8,6 +8,22 @@ var current_focus := Node
 var lock_input := false
 
 
+func _input(event: InputEvent):
+    if event.is_action_pressed("toggle_full_screen"):
+        get_viewport().set_input_as_handled()
+        _swap_fullscreen_mode()
+    elif event.is_action_pressed("quit_game"):
+        get_viewport().set_input_as_handled()
+        get_tree().quit()
+
+
+func _swap_fullscreen_mode():
+    if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+    else:
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+
 func set_focus(new_focus):
     if current_focus:
         lose_focus.emit(current_focus, new_focus)
