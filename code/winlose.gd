e4632f465fc9b84647385791a26a36d27300e914
David Briscoe <idbrii@gmail.com>
2024-12-20 21:25:25 -0800

    Hack fix crash when winning

--- code/winlose.gd
+++ code/winlose.gd
@@ -16,4 +16,8 @@ func _on_died():
 
 func _on_all_enemies_defeated():
     await get_tree().create_timer(3).timeout
-    get_tree().change_scene_to_file("res://scenes/title_gameover_win.tscn")
+    var tree = get_tree()
+    if tree:
+        tree.change_scene_to_file("res://scenes/title_gameover_win.tscn")
+    # else already transitioning?
+    # sometimes we get here with nil tree. maybe because other creatures are defeated at the same time as boss?
