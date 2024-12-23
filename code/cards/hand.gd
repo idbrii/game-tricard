0b0c9739d617176d3f7038a6ac54b4e0e5b9c5f4
David Briscoe <idbrii@gmail.com>
2024-12-23 14:36:13 -0800

    Limit distance between cards in fan
    Fix cards have lots of weird spacing when you only have 3.
    
    Set a limit between the cards so they're never too far apart. Don't want
    to always use this limit so when you have lots of cards, they still all
    fit on screen.
    
    Test
    * 2 cards in hand.
    * 15 cards in hand.

--- code/cards/hand.gd
+++ code/cards/hand.gd
@@ -8,6 +8,8 @@ extends Control
 
 ## The fraction of a circle used for the card fan. 0 would make them a flat line.
 @export_range(0.0, 1.0) var card_fan_angle_turns := 0.25
+## Limit the fraction of a circle between each card in the fan.
+@export_range(0.0, 1.0) var card_fan_turns_per_card_max := 0.035
 
 ## When a card is selected, move it up a bit.
 @export_range(0.0, 3.0) var selected_offset_multiplier := 1.4
@@ -23,7 +25,13 @@ func _process(_dt: float):
 func layout():
     var count = get_child_count()
 
-    var total_angle = card_fan_angle_turns * TAU
+    var total_angle = card_fan_angle_turns
+    if count > 0:
+        var per_card = card_fan_angle_turns / count
+        per_card = min(per_card, card_fan_turns_per_card_max)
+        total_angle = per_card * count
+    total_angle *= TAU
+
     var angle_delta = total_angle / count
     var angle_offset = -total_angle / 2.0 + angle_delta
 
